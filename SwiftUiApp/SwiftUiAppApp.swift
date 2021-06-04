553539edd8866af2304489d735dad956b5b02f35
AnnGolub <anka.golub17@gmail.com>
2021-06-04 11:10:34 +0300

    Initial Commit

---
+++ SwiftUiApp/SwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAppApp.swift
+//  SwiftUiApp
+//
+//  Created by Анна Голубева on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
